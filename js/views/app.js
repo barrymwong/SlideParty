369d9335df3a8aaa0628b07260dff42eb8291df4
Hack Reactor Students <github@hackreactor.com>
2013-07-23 22:51:35 -0700

    working on router.navigate

--- js/views/app.js
+++ js/views/app.js
@@ -15,11 +15,24 @@ var AppView = Backbone.View.extend({
 
     App.mainRouter = new MainRouter();
     Backbone.history.start();
+    App.mainRouter.navigate('slides/' + 1);
+  },
+
+  events: {
+    'keyup': 'keyUp'
+  },
+
+  keyUp: function(event) {
+    // left 37, right 39
+    if(event.keyCode === 37 || event.keyCode === 39){
+      App.Vent.trigger('changeSlide', {
+        direction: event.keyCode === 39 ? 'next': 'prev'
+      });
+    }
   },
 
   render: function() {
     this.$el.append(this.slidesView.render().el);
-
     return this;
   }
 });
--- js/views/slides.js
+++ js/views/slides.js
@@ -1,6 +1,48 @@
 var SlidesView = Backbone.View.extend({
   className: 'slides',
 
+  initialize: function() {
+    this.currentSlideIndex = 1;
+    App.Vent.on('init', this.hideAllButFirst, this);
+    App.Vent.on('changeSlide', this.changeSlide, this);
+  },
+
+  hideAllButFirst: function() {
+    this.$el.children(':nth-child(n+2)').hide();
+  },
+
+  changeSlide: function(options) {
+    var newSlide,
+        slide = this.$el.children();
+
+    if(this.currentSlideIndex > 0 ) {
+      if(options.slideIndex) {
+        this.currentSlideIndex = +options.slideIndex;
+      } else {
+        this.nextSlide(options.direction);
+      }
+
+      slide.hide();
+      slide.eq(this.currentSlideIndex - 1).show();
+    }
+  },
+
+  nextSlide: function(dir) {
+    var slide = this.$el.children(), 
+        go = 0;
+
+      go = dir === 'next' ? this.currentSlideIndex++ : this.currentSlideIndex--;
+
+    // boundries
+    go = go < 1 ? 1 : go > this.collection.length ? this.collection.length : go;
+
+    console.log(go);
+
+    App.mainRouter.navigate('slides/' + go);
+    slide.hide();
+    slide.eq(go).show();
+  },
+
   render: function() {
     this.$el.empty();
 
