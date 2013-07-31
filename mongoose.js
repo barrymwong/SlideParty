af19390db23cc9a36a59792dc88207292c5c218d
barry <barrymwong@yahoo.com>
2013-07-31 12:50:11 -0700

    successful mongoose test, can write to mongodb

--- mongoose.js
+++ mongoose.js
@@ -8,3 +8,24 @@ myDB.on('error', console.error.bind(console, 'connection error:'));
 myDB.once('open', function callback () {
   console.log("MY DB Connected with Mongoose");
 });
+
+var slideSchema = new mongoose.Schema(
+  {
+    image: String,
+    title: String
+  },
+  {
+    collection: 'test_insert2'
+  }
+);
+
+var Slide = mongoose.model('test_insert2', slideSchema);
+
+
+var slide = new Slide({title: 'cheeseburger'});
+var slide2 = new Slide({title: 'prime rib'});
+var slide3 = new Slide({title: 'buta kimchee'});
+
+slide.save(function(err, slide) {
+  slide.title;
+});
